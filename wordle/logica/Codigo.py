b42f08e14ec415377ff123d5bb9e54cb4f77b1ee
Tatiana <tatana442015@hotmail.com>
2023-09-20 11:37:52 -0500

    Se realizan cambios a la clase PalabraOculta

--- wordle/logica/Codigo.py
+++ wordle/logica/Codigo.py
@@ -1,37 +1,35 @@
 import random
 
+PALABRAS: list[str] = []
+
 
 class PalabraOculta:
     def __init__(self):
-        self.palabra: list = self.palabra_oculta()
-
-    def palabra_oculta(self):
-        palabras = ["", ""]
-        return random.choice(palabras)
-
-    def verificar_palabra(self, palabra_intento):
-        if palabra_intento in self.palabra_oculta():
+        self.palabra_oculta: str = PALABRAS[random.randint(0, len(PALABRAS))]
+    @staticmethod
+    def verificar_palabra(self, palabra_intento : str):
+        if palabra_intento in PALABRAS:
             return True
         else:
             return False
 
     def comparar_palabras(self, palabra_intento):
-        if palabra_intento == self.palabra:
-            pass
-        # se debe llamar jugador gano
+        if palabra_intento == self.palabra_oculta:
+            return True
         else:
-            self.retroalimentar(palabra_intento)
+            return False
 
     def retroalimentar(self, palabra_intento):
         retroalimentacion = ""
-        for i in range(len(self.palabra)):
-            if palabra_intento[i] == self.palabra[i]:
-                retroalimentacion += "\033[92m" + palabra_intento[i] + "\033[0m "  # Verde
-            elif palabra_intento[i] in self.palabra:
-                retroalimentacion += "\033[93m" + palabra_intento[i] + "\033[0m "  # Amarillo
-            else:
-                retroalimentacion += "\033[90m" + palabra_intento[i] + "\033[0m "  # Gris
-            print(retroalimentacion)
+        if  not self.comparar_palabras(palabra_intento):
+            for i in range(len(self.palabra_oculta)):
+                if palabra_intento[i] == self.palabra_oculta[i]:
+                    retroalimentacion += "\033[92m" + palabra_intento[i] + "\033[0m "  # Verde
+                elif palabra_intento[i] in self.palabra_oculta:
+                    retroalimentacion += "\033[93m" + palabra_intento[i] + "\033[0m "  # Amarillo
+                else:
+                    retroalimentacion += "\033[90m" + palabra_intento[i] + "\033[0m "  # Gris
+                print(retroalimentacion)
 
 
 class Jugador:
@@ -40,7 +38,7 @@ class Jugador:
         self.intentos: int = 0
         self.estadisticas: dict[str, int] = {}
 
-    def ingresar_palabra(self, palabra_intento) -> str:
+    def ingresar_palabra(self, palabra_intento: str) -> str:
         if PalabraOculta.verificar_palabra(palabra_intento):
             self.intento_realizado()
             return palabra_intento
@@ -57,6 +55,7 @@ class Jugador:
         else:
             self.estadisticas["Partidas perdidas"] += 1
 
+
     def jugador_gano(self) -> bool:
         if PalabraOculta.palabra_encontrada():
             return True
@@ -71,14 +70,14 @@ class Jugador:
 
 
 class Wordle:
+    """
     def registrar_jugador(self, nombre: str):
-        self.nombre_jugador = nombre
-
-    def mostrar_estadistica(self, mostrar: bool):
+        self.jugador = Jugador(self):
         self.mostrar_estadistica = mostrar
 
     def iniciar_juego(self, iniciar: bool):
         self.iniciar_juego = iniciar
 
     def significado_palabra(self):
-        pass
+    """
+    pass
