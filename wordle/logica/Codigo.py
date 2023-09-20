95212582bf617d1e80f30dbe1e8914406e490703
violrltl <131842762+violrltl@users.noreply.github.com>
2023-09-20 14:13:55 -0500

    Clase Wordle

--- wordle/logica/Codigo.py
+++ wordle/logica/Codigo.py
@@ -1,5 +1,4 @@
 import random
-from typing import Union
 
 PALABRAS: list[str] = []
 
@@ -7,23 +6,22 @@ PALABRAS: list[str] = []
 class PalabraOculta:
     def __init__(self):
         self.palabra_oculta: str = PALABRAS[random.randint(0, len(PALABRAS))]
-
     @staticmethod
-    def verificar_palabra(palabra_intento: str):
+    def verificar_palabra(self, palabra_intento : str):
         if palabra_intento in PALABRAS:
             return True
         else:
             return False
 
-    def comparar_palabras(self, palabra_intento: str):
+    def comparar_palabras(self, palabra_intento):
         if palabra_intento == self.palabra_oculta:
             return True
         else:
             return False
 
-    def retroalimentar(self, palabra_intento: str):
+    def retroalimentar(self, palabra_intento):
         retroalimentacion = ""
-        if not self.comparar_palabras(palabra_intento):
+        if  not self.comparar_palabras(palabra_intento):
             for i in range(len(self.palabra_oculta)):
                 if palabra_intento[i] == self.palabra_oculta[i]:
                     retroalimentacion += "\033[92m" + palabra_intento[i] + "\033[0m "  # Verde
@@ -40,48 +38,57 @@ class Jugador:
         self.intentos: int = 0
         self.estadisticas: dict[str, int] = {}
 
-    def ingresar_palabra(self, palabra_intento: str) -> Union[str, bool]:
+    def ingresar_palabra(self, palabra_intento: str) -> str:
         if PalabraOculta.verificar_palabra(palabra_intento):
             self.intento_realizado()
             return palabra_intento
         else:
-            return False
+            return f"Palabra no encontrada en el sistema."
 
     def intento_realizado(self):
         self.intentos += 1
 
-    def actualizar_estadisticas(self, palabra_intento: str):
-        if self.jugador_gano(palabra_intento):
+    def actualizar_estadisticas(self):
+        if self.jugador_gano():
             self.estadisticas[str(self.intentos)] += 1
             self.estadisticas["Partidas ganadas"] += 1
-            self.estadisticas["Racha"] += 1
-            self.estadisticas["Partidas jugadas"] += 1
-        elif self.jugador_perdio(palabra_intento):
+        else:
             self.estadisticas["Partidas perdidas"] += 1
-            self.estadisticas["Racha"] = 0
-            self.estadisticas["Partidas jugadas"] += 1
 
-    def jugador_gano(self, palabra_intento: str) -> bool:
-        palabra = self.ingresar_palabra(palabra_intento)
-        palabra_oculta = PalabraOculta()
-        if palabra_oculta.comparar_palabras(palabra):
+
+    def jugador_gano(self) -> bool:
+        if PalabraOculta.palabra_encontrada():
             return True
         else:
             return False
 
-    def jugador_perdio(self, palabra_intento: str) -> bool:
-        return self.intentos == 6 and not self.jugador_gano(palabra_intento)
+    def jugador_perdio(self) -> bool:
+        if self.intentos == 6 and not self.jugador_gano():
+            return True
+        else:
+            return False
 
 
 class Wordle:
-    """
     def registrar_jugador(self, nombre: str):
-        self.jugador = Jugador(self):
+        self.jugador = Jugador(self.nombre)
+        print(f"Bienvenido, {self.jugador.nombre}.")
+        nuevo_juego = input("¿Deseas empezar un nuevo juego? a: si, b: no ")
+        if nuevo_juego == "a":
+            self.iniciar_juego()
+        else:
+            pass
+    """""
         self.mostrar_estadistica = mostrar
 
     def iniciar_juego(self, iniciar: bool):
         self.iniciar_juego = iniciar
 
     def significado_palabra(self):
+        pass
+    def mostrar_instrucciones(bool):
+        pass
+    def finalizar_juego():
+        pass
     """
-    pass
+
