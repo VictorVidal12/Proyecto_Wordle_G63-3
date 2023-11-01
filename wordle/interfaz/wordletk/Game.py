029a1768f3b9c841b1862e3ae5559ec33d650d83
Tatiana <tatana442015@hotmail.com>
2023-11-01 10:45:06 -0500

    Se comienza a implementar la clase casilla

--- wordle/interfaz/wordletk/Game.py
+++ wordle/interfaz/wordletk/Game.py
@@ -6,31 +6,12 @@ from tkinter import Frame
 class Casilla(Frame):
     def __init__(self, master, fila, columna):
         super().__init__()
-        self.estado = "vacia"
+        self.estado = "vacia"  # Puede ser "vacia", "correcta" o "incorrecta"
         self.frame = Frame(master, width=50, height=50, bg="white", borderwidth=1, relief="solid")
         self.frame.grid(row=fila, column=columna)
         self.label = Label(self.frame, text="", font=("Arial", 24))
         self.label.pack(expand=True)
-
-    def cambiar_estado(self, estado):
-        self.estado = estado
-        colores = {"vacia": "white", "correcta": "green", "incorrecta": "red"}
-        self.frame.configure(bg=colores[estado])
-
-    def establecer_letra(self, letra):
-        self.label.configure(text=letra)
-
-class Tablero:
-    def __init__(self, filas, columnas):
-        self.tablero = Tk()
-        self.tablero.title(f"Tablero Wordle {filas}x{columnas}")
-        self.casillas = [[Casilla(self.tablero, i, j) for j in range(columnas)] for i in range(filas)]
-
-    def iniciar(self):
-        self.tablero.mainloop()
-
-tablero = Tablero(5, 6)
-tablero.iniciar()
+        self.letra_correcta = ""
 
 
 
