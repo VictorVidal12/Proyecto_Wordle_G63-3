cc4b8bf5c6d320b29902fc83b3f4925526a66d4a
Tatiana <tatana442015@hotmail.com>
2023-10-31 20:05:14 -0500

    Se enlazan la pagina de registro con el juego (HTML)

--- wordle/interfaz/web_app.py
+++ wordle/interfaz/web_app.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
-
 @app.route('/')
+def registro():
+    return render_template('registro.html', juego_url=url_for('juego'))
+
+
+@app.route('/Juego')
 def juego():
     return render_template('juego.html')
 
-@app.route('/registro')
-def registro():
-    return render_template('registro.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5017)
-
