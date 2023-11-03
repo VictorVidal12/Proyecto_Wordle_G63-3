d9f92477277e1ad5d1b3418e57eb54df5073d7a2
VictorVidal12 <victormanuelvb12@gmail.com>
2023-11-02 19:03:21 -0500

    Actualización del método de gestión de la API

--- wordle/logica/palabras.py
+++ wordle/logica/palabras.py
@@ -1,20 +1,44 @@
+import json
+
 import requests
-import ast
 
 """
 Con este código se hace el llamado a la API y se obtienen las palabras de allí 
 
 """
-api_url = 'https://api.api-ninjas.com/v1/randomword'
-n = 0
-f = open("palabras", "w")
-while n < 10000:
-    response = requests.get(api_url, headers={'X-Api-Key': '74w5GflDZj4lbxnx3l7Lrg==129rRPt1vTEb5hdg'})
-    plb = ast.literal_eval(response.text)
-    if len(plb['word']) == 5:
-        palabras_wordle: str = plb['word']
-        f.write(palabras_wordle + '\n')
-    n += 1
 
-f.close()
 
+def get_random_word(length):
+    random_word_api_url = "https://random-word-api.herokuapp.com/word"
+    random_word_api_response = requests.get(
+        random_word_api_url, params={"length": length, "lang": "en"})
+
+    random_word = random_word_api_response.json()[0]
+    return random_word
+
+
+def get_word_definition(word):
+    url = "https://api.dictionaryapi.dev/api/v2/entries/en/{}"
+    response = requests.get(url.format(word))
+    return response
+
+
+def get_randoms_words_with_meanings(length):
+    words = {}
+    for _ in range(101):
+        status_code = 404
+        while status_code != 200:
+            word = get_random_word(length)
+            meaning_response = get_word_definition(word)
+            status_code = meaning_response.status_code
+        definition = meaning_response.json()[0]["meanings"][0]["definitions"][0]["definition"]
+        random_word = {
+            'word': word,
+            'definition': definition}
+        words[word] = random_word['word']
+        words[f'{word} definition'] = random_word['definition']
+    return words
+
+
+rdw = get_randoms_words_with_meanings(5)
+print(rdw)
